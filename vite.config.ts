dce2231035a580d89f268e4ef3bf89b9f09f6f40
Raya Gabry <raya.gabry@gmail.com>
2026-04-19 14:43:07 -0400

    vite fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/rumikube-puzzle/',
+  base: '/rumm-puzz/',
   plugins: [
     react(),
     VitePWA({
