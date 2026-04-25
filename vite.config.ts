114f2ef0ae0133c6d773d7d4e63fc7311a032206
Raya Gabry <raya.gabry@gmail.com>
2026-04-25 19:40:12 -0400

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/rumm-puzz/',
+  server: {
+    host: true,
+    allowedHosts: ['.local'],
+  },
   plugins: [
     react(),
     VitePWA({
